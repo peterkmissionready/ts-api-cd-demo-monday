3e3497a9852e0fcab1b79d0fd07262ce9a52be9f
peterkmissionready <131961491+peterkmissionready@users.noreply.github.com>
2023-05-25 16:02:27 +1200

    added test runs from my past Mission 3 assignments.

--- src/tests/unit/taskController.spec.ts
+++ src/tests/unit/taskController.spec.ts
@@ -1,4 +1,4 @@
-import { getAllTasks, getOneTask } from '../../controllers/taskController'
+import { CalculateRiskRating, getAllTasks, getOneTask } from '../../controllers/taskController'
 import { createRequest, createResponse } from 'node-mocks-http'
 
 describe('getAllTasks', () => {
@@ -47,6 +47,59 @@ describe('getOneTask', () => {
     expect(res.json()._getData()).toEqual(expected)
 
   })
+})
+
+describe('Calculate Risk Rating', () =>{
+  /*const inputs=["My only car accident was when I finished turning left, the car behind ended bumping the rear of my car.",
+    222222222,
+    "",
+    "        ",
+    "*-/+0(#@|]",
+    "Bump and crash",
+    "crashcrashcrash",
+    "2017 had 97 fatal crashes.",
+    null
+  ]
+
+  const outputs=["1",
+    "Error- not a string",
+    "Error- empty string",
+    "has only whitespace",
+    "Error- doesn't contain a letter",
+    "2",
+    "3",
+    "1"
+  ]
+  */
+
+  test('what happened',() =>{
+    expect(CalculateRiskRating("My only car accident was when I turned left, the car behind ended bumping the rear of my car")).toBe(1);
+  });
+  
+  test('Numbers only',() =>{
+      expect(CalculateRiskRating(22222)).toBe("Error- not a string");
+  });
+  test('empty string',() =>{
+      expect(CalculateRiskRating("")).toBe("Error- empty string");
+  });
+  
+  test('Whitespace only',() =>{
+      expect(CalculateRiskRating("        ")).toBe("Error- has only whitespace");
+  });
+  test('No letters',() =>{
+    expect(CalculateRiskRating("*-/+0(#@|]\\")).toBe("Error- doesn't contain a letter");
+  });
+  
+  test('Title of a movie',() =>{
+    expect(CalculateRiskRating("bump and crash")).toBe(2);
+  });
+  
+  test('expect not a string',() =>{
+    expect(CalculateRiskRating(null)).toBe("Error- not a string");
+  });
+
+
+
 })
  /*
 import { getMockReq, getMockRes } from '@jest-mock/express'
